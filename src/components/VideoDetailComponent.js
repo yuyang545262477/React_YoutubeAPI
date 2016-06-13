f9e00b5a0933e56c765c885f9e726489548d64cf
David_Yu <yuyang545262477@gmail.com>
2016-06-14 00:05:30 +0800

    finish videoDetail component ..

--- src/components/VideoDetailComponent.js
+++ src/components/VideoDetailComponent.js
@@ -6,8 +6,32 @@ import React from "react";
 
 class VideoDetailComponent extends React.Component {
     render() {
+        const video = this.props.video;
+        if (!video) {
+            return (
+                <div>
+                    Loading.....
+                </div>
+            )
+        }
+        const videoId = video.id.videoId;
+        const url = `https://www.youtube.com/embed/${videoId}`;
         return (
-            <div></div>
+            <div className="video-detail col-md-8">
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe src={url} frameborder="0" className="embed-responsive-item"></iframe>
+                </div>
+                <div className="details">
+                    <div>
+                        {video.snippet.title}
+                    </div>
+                    <div>
+                        {video.snippet.description}
+                    </div>
+                </div>
+            
+            
+            </div>
         )
     }
 }
